32011867c753a4843ee81f030b25b2fa9d99f4db
Manabu Niseki <manabu.niseki@gmail.com>
2024-10-05 10:11:29 +0900

    wip

--- web-components/template-components/index.ts
+++ web-components/template-components/index.ts
@@ -3,15 +3,15 @@ import { toggleVisibility, onLoad } from "./src/template-utils";
 
 const Modal = defineAsyncComponent(() => import("./src/Modal.ce.vue"));
 const SearchQuery = defineAsyncComponent(
-    () => import("./src/SearchQuery.ce.vue"),
+  () => import("./src/SearchQuery.ce.vue"),
 );
 
 customElements.define("ar-modal", defineCustomElement(Modal));
 customElements.define("ar-search-query", defineCustomElement(SearchQuery));
 
 const templateUtils = {
-    toggleVisibility,
-    onLoad,
+  toggleVisibility,
+  onLoad,
 };
 
 export { templateUtils };
