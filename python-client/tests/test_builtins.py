e4423413fb75113ad1cc98138366b733134942de
Manabu Niseki <manabu.niseki@gmail.com>
2024-10-06 10:14:15 +0900

    test: test report generation with built-in demo (#9)

---
+++ python-client/tests/test_builtins.py
@@ -0,0 +1,18 @@
+from html.parser import HTMLParser
+
+import pytest
+
+import arakawa as ar
+
+from .builtins import demo
+
+
+@pytest.fixture
+def parser():
+    return HTMLParser()
+
+
+def test_demo(parser: HTMLParser):
+    report = ar.Report(demo())
+    html = report.stringify()
+    assert parser.feed(html) is None
